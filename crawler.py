2359c3a286a3e43ff6c17b12bc56697dfad9a1ef
naomikalfa <79673207+naomikalfa@users.noreply.github.com>
2021-03-11 17:07:04 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ ROOT_URL = 'https://news.ycombinator.com/'
 HN_LINK_URL = 'https://news.ycombinator.com/item?id='
 
 
-def fetch_page(url):
+def fetch_page(url, greater_than):
 
     high_comment_articles = []
 
@@ -53,20 +53,25 @@ def fetch_page(url):
         int_stripped_and_strained = int(clean)
         fresh_comment_soup = int_stripped_and_strained
 
-        if fresh_comment_soup > 100:
+        if fresh_comment_soup > greater_than:
             high_comment_articles.append([title.text, refined_links, hn_link_strained, fresh_comment_soup])
 
     return high_comment_articles
 
 
+greater_than = 100
 pages_to_search = 30
 stack = []
 
 for i in range(pages_to_search):
-    stack.extend(fetch_page(ROOT_URL + '/news?p=' + str(i)))
+    stack.extend(fetch_page(ROOT_URL + '/news?p=' + str(i), greater_than))
 
 for article in stack:
     print(Fore.LIGHTBLUE_EX + article[0])
     print(Fore.BLUE + '    ' + article[1])
     print(Fore.CYAN + '    ' + article[2])
     print(Fore.YELLOW + '    ' + 'comments:', article[3], '\n')
+    
+print(Fore.RED + '∴ We searched through', pages_to_search, 'pages on', Fore.LIGHTRED_EX + 'HN & sifted through',
+      pages_to_search * 30, Fore.LIGHTMAGENTA_EX + 'links & found a total', Fore.MAGENTA + 'of', len(stack),
+      'articles with >', Fore.BLUE + str(greater_than), 'comments - - Happy reading! ∴', '\n')
